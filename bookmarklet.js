7b4dfb91fa7d0ae2fbaa4a1a345eb54280ca41f9
Ndh4k4 <ndh4k423@gmail.com>
2018-10-14 20:10:07 +1100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,11 +5,3 @@
 		paragraphs[i].innerHTML = '';
     }
 })();
-
-
-
-function main() { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } }main();
-
-
-(function() { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } }();
-
